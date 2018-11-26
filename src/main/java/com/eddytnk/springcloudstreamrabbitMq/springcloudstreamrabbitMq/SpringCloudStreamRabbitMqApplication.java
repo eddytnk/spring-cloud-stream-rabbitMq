cd840ff99ab59081072e85572b2774526fe28828
Edward Tanko <eddytnk@gmail.com>
2018-11-25 21:22:39 -0600

    project structure set up

---
+++ src/main/java/com/eddytnk/springcloudstreamrabbitMq/springcloudstreamrabbitMq/SpringCloudStreamRabbitMqApplication.java
@@ -0,0 +1,12 @@
+package com.eddytnk.springcloudstreamrabbitMq.springcloudstreamrabbitMq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamRabbitMqApplication.class, args);
+	}
+}
